f31ded76d541828b57648ccbc929f4b4d54d303d
Robbie Jakob-Whitworth <git@robbie.dev>
2022-11-12 17:42:48 +0000

    Display alert and block in-app browser for Apple Pay

--- components/buttons/applepay/ApplePay.js
+++ components/buttons/applepay/ApplePay.js
@@ -1,30 +1,83 @@
 import {useEffect, useState} from "react";
 import styles from './applepay.module.css'
 import TextWithOptionalAmount from "../../textwithoptionalamount/TextWithOptionalAmount";
+import InApp from "../../../utils/InApp";
+import Swal from 'sweetalert2'
 import Image from "next/image";
 import applePayLogo from '../../../assets/img/apple-pay.png'
 
-const ApplePay = ({amount, buttonClassName}) => {
-    const [hasApplePay, setHasApplePay] = useState(false);
-    useEffect(() => {
-        setHasApplePay(!(typeof window !== 'undefined' && !window.ApplePaySession))
+function applePayWarning() {
+    Swal.fire({
+        title: "You can't use Apple Pay",
+        html: "<p>You can't use Apple Pay when you view this page in an in-app browser, Facebook Messenger. <strong>Open this page in Safari to use Apple Pay.</strong></p>",
+        icon: "warning",
+        confirmButtonText: "I don't want to use Apple Pay",
+        footer: "<p><strong>Psst:</strong> Want to use Apple Pay? Open this page in Safari.</p>"
     })
+}
+
+const ApplePay = ({amount, buttonClassName}) => {
+    const [hasApplePay, setHasApplePay] = useState("unavailable");
+
+    useEffect(() => {
+        const inapp = new InApp(navigator.userAgent || navigator.vendor || window.opera);
+        if (inapp.isInApp && inapp.iOS) {
+        // if (true) {
+            setHasApplePay("disabled")
+            applePayWarning()
+        } else if (window.ApplePaySession) {
+            setHasApplePay("available")
+        } else {
+            setHasApplePay("unavailable")
+        }
+    })
+    let applePayVisibleClassName;
+    switch (hasApplePay) {
+        case "available": {
+            applePayVisibleClassName = null
+            break
+        }
+        case "disabled": {
+            applePayVisibleClassName = styles.disabled
+            break
+        }
+        case "unavailable": {
+            applePayVisibleClassName = styles.unavailable
+            break
+        }
+        default: {
+            applePayVisibleClassName = null
+        }
+    }
+
+    const applePayClick = (e) => {
+        if (hasApplePay === "disabled") {
+            e.preventDefault()
+            applePayWarning()
+        } else {
+            return true
+        }
+    }
+
     return (
-        <a
-            href={TextWithOptionalAmount({
-                text: "https://settleup.starlingbank.com/robertjakobwhitworth?amount=",
-                amount,
-                prefix: ""
-            })}
-            className={[
-                'btn',
-                styles.applepay,
-                buttonClassName,
-                hasApplePay ? '' : styles.hidden
-            ].join(' ')}
-        >
-            <Image src={applePayLogo} alt={"Apple Pay"} width={212} placeholder="blur" />
-        </a>
+        <>
+            <a
+                href={TextWithOptionalAmount({
+                    text: "https://settleup.starlingbank.com/robertjakobwhitworth?amount=",
+                    amount,
+                    prefix: ""
+                })}
+                className={[
+                    'btn',
+                    styles.applepay,
+                    buttonClassName,
+                    applePayVisibleClassName
+                ].join(' ')}
+                onClick={(e) => applePayClick(e)}
+            >
+                <Image src={applePayLogo} alt={"Apple Pay"} width={212} placeholder="blur"/>
+            </a>
+        </>
     )
 }
 
---
+++ utils/InApp.js
@@ -0,0 +1,68 @@
+// Taken from https://github.com/f2etw/detect-inapp/blob/b82a67e7293ebb7534210770b9c85cc6617fa44d/src/inapp.js
+// Used with MIT license on 2021-01-05
+
+export default class InApp {
+    constructor(userAgent) {
+        this.ua = userAgent || "";
+        this.browserList = {
+            messenger: /\bFB[\w_]+\/(Messenger|MESSENGER)/,
+            facebook: /\bFB[\w_]+\//,
+            twitter: /\bTwitter/i,
+            line: /\bLine\//i,
+            wechat: /\bMicroMessenger\//i,
+            puffin: /\bPuffin/i,
+            miui: /\bMiuiBrowser\//i,
+            instagram: /\bInstagram/i,
+            chrome: /\bCrMo\b|CriOS|Android.*Chrome\/[.0-9]* (Mobile)?/,
+            safari: /Version.*Mobile.*Safari|Safari.*Mobile|MobileSafari/,
+            ie: /IEMobile|MSIEMobile/,
+            firefox: /fennec|firefox.*maemo|(Mobile|Tablet).*Firefox|Firefox.*Mobile|FxiOS/,
+        };
+    }
+
+    findKey(obj, target) {
+        for (let browser in this.browserList) {
+            // Double-check that `browser` is actually in the list of browsers
+            if (!this.browserList.hasOwnProperty(browser)) {
+                continue;
+            }
+
+            // Grab the regex expression
+            const regex = this.browserList[browser];
+
+            // If the expression matches, then we've found our browser
+            if (target.test(regex)) {
+                return browser;
+            }
+        }
+
+        // If no matches, we found nothing
+        return false;
+    }
+
+    get browser() {
+        return this.findKey(this.browserList, this.ua) || 'other';
+    }
+
+    get isMobile() {
+        return /(iPad|iPhone|Android|Mobile)/i.test(this.ua) || false;
+    }
+
+    get iOS() {
+        return /(iPad|iPhone)/i.test(this.ua) || false;
+    }
+
+    get isDesktop() {
+        return !this.isMobile;
+    }
+
+    get isInApp() {
+        const rules = [
+            'WebView',
+            '(iPhone|iPod|iPad)(?!.*Safari\/)',
+            'Android.*(wv|\.0\.0\.0)',
+        ];
+        const regex = new RegExp(`(${rules.join('|')})`, 'ig');
+        return !!this.ua.match(regex);
+    }
+}
