5b3d3667f38deb5af58740c9aaf0a9d0fecd9dc6
Robbie Jakob-Whitworth <git@robbie.dev>
2022-11-12 15:13:05 +0000

    Correctly show/hide Apple Pay button based on browser support

--- nextjs/components/buttons/applepay/ApplePay.js
+++ nextjs/components/buttons/applepay/ApplePay.js
@@ -1,12 +1,14 @@
+import {useEffect, useState} from "react";
 import styles from './applepay.module.css'
 import TextWithOptionalAmount from "../../textwithoptionalamount/TextWithOptionalAmount";
 import Image from "next/Image";
 import applePayLogo from '../../../assets/img/apple-pay.png'
 
 const ApplePay = ({amount, buttonClassName}) => {
-    if (typeof window !== 'undefined' && !window.ApplePaySession) {
-        return null
-    }
+    const [hasApplePay, setHasApplePay] = useState(false);
+    useEffect(() => {
+        setHasApplePay(!(typeof window !== 'undefined' && !window.ApplePaySession))
+    })
     return (
         <a
             href={TextWithOptionalAmount({
@@ -14,7 +16,12 @@ const ApplePay = ({amount, buttonClassName}) => {
                 amount,
                 prefix: ""
             })}
-            className={['btn', styles.applepay, buttonClassName].join(' ')}
+            className={[
+                'btn',
+                styles.applepay,
+                buttonClassName,
+                hasApplePay ? '' : styles.hidden
+            ].join(' ')}
         >
             <Image src={applePayLogo} alt={"Apple Pay"} width={212} />
         </a>
