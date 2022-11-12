6c8f153f72543756ab5dc3fddd8ea478b5cb023f
Robbie Jakob-Whitworth <git@robbie.dev>
2022-11-12 16:36:13 +0000

    Add favicon <link> tag

--- pages/_document.js
+++ pages/_document.js
@@ -4,13 +4,14 @@ export default function Document() {
     return (
         <Html>
             <Head>
+                <link rel="icon" type="image/x-icon" href="/favicon.ico" />
                 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
                       integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
                       crossOrigin="anonymous" />
             </Head>
             <body>
-            <Main />
-            <NextScript />
+                <Main />
+                <NextScript />
             </body>
         </Html>
     )
