91c3c3475fb4efd61ecb03d4faa650fd27672be3
Robbie Jakob-Whitworth <git@robbie.dev>
2022-11-12 14:56:36 +0000

    Add pay by Apple Pay

---
+++ nextjs/components/buttons/applepay/ApplePay.js
@@ -0,0 +1,24 @@
+import styles from './applepay.module.css'
+import TextWithOptionalAmount from "../../textwithoptionalamount/TextWithOptionalAmount";
+import Image from "next/Image";
+import applePayLogo from '../../../assets/img/apple-pay.png'
+
+const ApplePay = ({amount, buttonClassName}) => {
+    if (!window.ApplePaySession) {
+        return null
+    }
+    return (
+        <a
+            href={TextWithOptionalAmount({
+                text: "https://settleup.starlingbank.com/robertjakobwhitworth?amount=",
+                amount,
+                prefix: ""
+            })}
+            className={['btn', styles.applepay, buttonClassName].join(' ')}
+        >
+            <Image src={applePayLogo} alt={"Apple Pay"} width={212} />
+        </a>
+    )
+}
+
+export default ApplePay;
--- nextjs/components/paymentbuttons/PaymentButtons.js
+++ nextjs/components/paymentbuttons/PaymentButtons.js
@@ -1,9 +1,11 @@
 import styles from './paymentbuttons.module.css'
 import PayPal from "../buttons/paypal/PayPal";
+import ApplePay from "../buttons/applepay/ApplePay";
 
 const PaymentButtons = ({amount}) => {
     return (
         <section className={styles.buttons}>
+            <ApplePay amount={amount} buttonClassName={styles.button} />
             <PayPal amount={amount} btnClassName={styles.btn} />
         </section>
     )
