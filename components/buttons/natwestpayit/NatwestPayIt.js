9cee761cf35f053c5818edd9fda92a8620179bac
Robbie Jakob-Whitworth <git@robbie.dev>
2024-04-01 12:35:07 +0000

    Add NatwestPayIt

---
+++ components/buttons/natwestpayit/NatwestPayIt.js
@@ -0,0 +1,21 @@
+import styles from "./natwestpayit.module.css";
+import TextWithOptionalAmount from "../../textwithoptionalamount/TextWithOptionalAmount";
+
+const NatwestPayIt = ({ amount, buttonClassName }) => {
+  return (
+    <a
+      href={TextWithOptionalAmount({
+        text: "https://paymentrequest.natwestpayit.com/reusable-links/f7ed2bd1-e39a-41fd-ab7b-faaba81b81d2",
+        amount,
+        prefix: "",
+      })}
+      className={["btn", styles.debitcard, buttonClassName, "btn-primary"].join(
+        " "
+      )}
+    >
+      Pay by Bank Transfer
+    </a>
+  );
+};
+
+export default NatwestPayIt;
